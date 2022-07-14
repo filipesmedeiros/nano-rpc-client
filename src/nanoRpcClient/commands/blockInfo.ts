6b2b0c4132d7626c3ef00f1aa51b1c9a2977ff26
Filipe Medeiros <hello@filipesm.com>
2022-07-14 12:14:29 +0100

    block_info

--- src/nanoRpcClient/commands/blockInfo.ts
+++ src/nanoRpcClient/commands/blockInfo.ts
@@ -1,21 +1,38 @@
+import { BlockType } from '../../types/types'
 import { NanoFetcher } from '../fetcher'
 
 export interface BlockInfoResponse {
-  RESPONSE_TYPE: any
+  block_account: string
+  amount: bigint
+  balance: bigint
+  height: bigint
+  local_timestamp: bigint
+  successor: string
+  confirmed: boolean
+  contents: {
+    type: 'state'
+    account: string
+    previous: string
+    representative: string
+    balance: bigint
+    link: string
+    link_as_account: string
+    signature: string
+    work: string
+  }
+  subtype: BlockType
 }
 
 export default function blockInfo(
   this: NanoFetcher,
-  MAIN_ARG: string,
-  OPTIONAL_ARGS: {},
+  hash: string,
   requestOptions?: { abortSignal: AbortSignal }
 ) {
   return this.fetch<BlockInfoResponse>(
     {
       action: 'block_info',
       data: {
-        MAIN_ARG,
-        ...OPTIONAL_ARGS,
+        hash,
       },
     },
     requestOptions
