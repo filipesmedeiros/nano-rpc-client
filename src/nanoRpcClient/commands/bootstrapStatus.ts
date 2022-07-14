072266c1ec65fa574906156d838fb20f30d7f53c
Filipe Medeiros <hello@filipesm.com>
2022-07-14 12:56:10 +0100

    bootstrap_status

--- src/nanoRpcClient/commands/bootstrapStatus.ts
+++ src/nanoRpcClient/commands/bootstrapStatus.ts
@@ -1,22 +1,42 @@
 import { NanoFetcher } from '../fetcher'
 
 export interface BootstrapStatusResponse {
-  RESPONSE_TYPE: any
+  bootstrap_threads: bigint
+  running_attempts_count: bigint
+  total_attempts_count: bigint
+  connections: {
+    clients: bigint
+    connections: bigint
+    idle: bigint
+    target_connections: bigint
+    pulls: bigint
+  }
+  attempts: {
+    id: string
+    mode: 'lazy' | 'legacy'
+    started: boolean
+    pulling: bigint
+    total_blocks: bigint
+    requeued_pulls: bigint
+    lazy_blocks: bigint
+    lazy_state_backlog: bigint
+    lazy_balances: bigint
+    lazy_destinations: bigint
+    lazy_undefined_links: bigint
+    lazy_pulls: bigint
+    lazy_keys: bigint
+    lazy_key_1: string
+    duration: bigint
+  }[]
 }
 
 export default function bootstrapStatus(
   this: NanoFetcher,
-  MAIN_ARG: string,
-  OPTIONAL_ARGS: {},
   requestOptions?: { abortSignal: AbortSignal }
 ) {
   return this.fetch<BootstrapStatusResponse>(
     {
       action: 'bootstrap_status',
-      data: {
-        MAIN_ARG,
-        ...OPTIONAL_ARGS,
-      },
     },
     requestOptions
   )
