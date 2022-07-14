97f8630d3e99b75de65da5e245f3b84d98982367
Filipe Medeiros <hello@filipesm.com>
2022-07-14 12:49:19 +0100

    bootstrap_any

--- src/nanoRpcClient/commands/bootstrapAny.ts
+++ src/nanoRpcClient/commands/bootstrapAny.ts
@@ -1,22 +1,16 @@
 import { NanoFetcher } from '../fetcher'
 
 export interface BootstrapAnyResponse {
-  RESPONSE_TYPE: any
+  success: ''
 }
 
 export default function bootstrapAny(
   this: NanoFetcher,
-  MAIN_ARG: string,
-  OPTIONAL_ARGS: {},
   requestOptions?: { abortSignal: AbortSignal }
 ) {
   return this.fetch<BootstrapAnyResponse>(
     {
       action: 'bootstrap_any',
-      data: {
-        MAIN_ARG,
-        ...OPTIONAL_ARGS,
-      },
     },
     requestOptions
   )
