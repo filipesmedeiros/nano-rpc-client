0279596a1b19276b63ea7e99f943c1305d90a2f4
Filipe Medeiros <hello@filipesm.com>
2022-07-14 13:02:15 +0100

    confirmation_height_currently_processing

--- src/nanoRpcClient/commands/confirmationHeightCurrentlyProcessing.ts
+++ src/nanoRpcClient/commands/confirmationHeightCurrentlyProcessing.ts
@@ -1,22 +1,16 @@
 import { NanoFetcher } from '../fetcher'
 
 export interface ConfirmationHeightCurrentlyProcessingResponse {
-  RESPONSE_TYPE: any
+  hash: string
 }
 
 export default function confirmationHeightCurrentlyProcessing(
   this: NanoFetcher,
-  MAIN_ARG: string,
-  OPTIONAL_ARGS: {},
   requestOptions?: { abortSignal: AbortSignal }
 ) {
   return this.fetch<ConfirmationHeightCurrentlyProcessingResponse>(
     {
       action: 'confirmation_height_currently_processing',
-      data: {
-        MAIN_ARG,
-        ...OPTIONAL_ARGS,
-      },
     },
     requestOptions
   )
