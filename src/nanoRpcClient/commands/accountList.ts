d8299e511df41d517c3ab42d62684181ec6148bc
Filipe Medeiros <hello@filipesm.com>
2022-07-24 16:13:28 +0100

    account_list

--- src/nanoRpcClient/commands/accountList.ts
+++ src/nanoRpcClient/commands/accountList.ts
@@ -1,21 +1,19 @@
 import { NanoFetcher } from '../fetcher'
 
 export interface AccountListResponse {
-  RESPONSE_TYPE: any
+  accounts: string[]
 }
 
 export default function accountList(
   this: NanoFetcher,
-  MAIN_ARG: string,
-  options?: {},
+  wallet: string,
   requestOptions?: { abortSignal: AbortSignal }
 ) {
   return this.fetch<AccountListResponse>(
     {
       action: 'account_list',
       data: {
-        MAIN_ARG,
-        ...options,
+        wallet,
       },
     },
     requestOptions
