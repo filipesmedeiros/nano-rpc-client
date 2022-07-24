f48f35fb8a6a7758cad64653fc5ef37894e9a393
Filipe Medeiros <hello@filipesm.com>
2022-07-24 16:11:00 +0100

    work_validate

--- src/nanoRpcClient/commands/workValidate.ts
+++ src/nanoRpcClient/commands/workValidate.ts
@@ -1,20 +1,27 @@
 import { NanoFetcher } from '../fetcher'
 
-export interface WorkValidateResponse {
-  RESPONSE_TYPE: any
-}
+export type WorkValidateResponse<Difficulty extends boolean | undefined> = {
+  valid_all: 1 | 0
+  valid_receive: 1 | 0
+  difficulty: string
+  multiplier: number
+} & (Difficulty extends true ? { valid: 1 | 0 } : {})
 
 export default function workValidate(
   this: NanoFetcher,
-  MAIN_ARG: string,
-  options?: {},
+  params: { work: string; hash: string },
+  options: { difficulty?: string; multiplier?: number } = {},
   requestOptions?: { abortSignal: AbortSignal }
 ) {
-  return this.fetch<WorkValidateResponse>(
+  return this.fetch<
+    WorkValidateResponse<
+      typeof options['difficulty'] extends true ? true : false
+    >
+  >(
     {
       action: 'work_validate',
       data: {
-        MAIN_ARG,
+        ...params,
         ...options,
       },
     },
